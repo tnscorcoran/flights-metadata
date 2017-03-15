5e5bbb2471cf273c0af9c9c416d3086aec3f0447
Tom Corcoran <tnscorcoran@gmail.com>
2017-03-15 10:36:26 -0400

    Re-org of packages

--- src/main/java/com/aviation/poc1/LocationsController.java
+++ src/main/java/com/aviation/poc1/LocationsController.java
@@ -1,4 +1,4 @@
-package com.aviation;
+package com.aviation.poc1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,46 +16,9 @@ import com.model.MetaData;
 import com.model.MetaDataLocation;
 
 @Controller
-public class MetadataController {
+public class LocationsController {
 	   
 	
-	@RequestMapping(value="/dirrestweb/rest/carriers", method=RequestMethod.GET, produces="application/json") 
-    public @ResponseBody List<MetaData> getCarriers() {
-		
-		List<MetaData> carriers = new ArrayList<MetaData>();
-		carriers.add(new MetaData("#1", "VENDOR CODE TEST"));
-		carriers.add(new MetaData("$1", "VENDOR CODE TEST"));
-		
-    	return carriers;   	
-    }
-	
-	@RequestMapping(value="/dirrestweb/rest/carrier/{id}", method=RequestMethod.GET, produces="application/json") 
-    public @ResponseBody MetaData getCarrier(@PathVariable("id") String id) {
-		
-		MetaData carrier = new MetaData(id, "Q DATA SET NAME PROJECT");
-				
-    	return carrier;
-    	
-    }
-	
-	@RequestMapping(value="/dirrestweb/rest/carriers/search/{id}", method=RequestMethod.GET, produces="application/json") 
-    public @ResponseBody Map<String, List<MetaData>> getCarrierSearch(@PathVariable("id") String id) {
-
-		Map<String, List<MetaData>> mapListMetaData = new HashMap <String, List<MetaData>>();
-
-		List<MetaData> carriers = new ArrayList<MetaData>();		
-		carriers.add(new MetaData("00", "Q DATA SET NAME PROJECT"));
-		carriers.add(new MetaData("001", "THREE POSITION VENDOR CODE"));
-		carriers.add(new MetaData("004", "WORL"));
-		carriers.add(new MetaData("007", "DOJ TEST VENDOR"));
-		mapListMetaData.put("byCode", carriers);
-		
-		carriers = new ArrayList<MetaData>();
-		mapListMetaData.put("byDesc", carriers);
-		
-    	return mapListMetaData;
-    	
-    }
 	
 	@RequestMapping(value="/dirrestweb/rest/cities", method=RequestMethod.GET, produces="application/json") 
     public @ResponseBody List<MetaData> getCities() {
